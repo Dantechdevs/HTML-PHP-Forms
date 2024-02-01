0ad858ec9dccf1119fe906988dfae062c2fc3826
Daniel Ngwasi <damnngwasi@gmail.com>
2024-02-01 14:05:26 +0300

    update

--- index.php
+++ index.php
@@ -8,7 +8,30 @@
 <body>
    <h1>HTML FORMS</h1>
    <div class="container">
-    
+    <form action="">
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Enter your name">
+</div>
+<div>
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Enter your email">
+</div>
+<div>
+            <label>Gender</label>
+            <input type="radio" name="gender">Male
+            <input type="radio" name="gender">Female
+            <input type="radio" name="gender">Others
+</div>
+<div>
+            <label>Moble</label>
+            <input type="text" name="mobile" placeholder="Enter your Phone Number">  
+</div>
+<div>
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Enter your Pasword">  
+</div>
+    </form>
    </div> 
 </body>
 </html>
